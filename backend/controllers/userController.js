884837bffc5c617251f2da788d271011cd818780
apple <apple@apples-MacBook-Pro.local>
2023-01-04 23:17:12 +0530

    create login controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 // Generate token
 const generateToken = (id) => {
@@ -24,6 +25,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
   // Check if email already exits
   const userExits = await User.findOne({ email });
+
   if (userExits) {
     res.status(400);
     throw new Error("User already exits");
@@ -67,8 +69,55 @@ const registerUser = asyncHandler(async (req, res) => {
 
 // Login user
 
-const loginUser = asyncHandler((req, res) => {
-  res.send("Login user");
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  console.log(req.body);
+  // Basic validation
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Please add email and password");
+  }
+
+  // Check if user exists
+  const user = await User.findOne({ email });
+  console.log("---user--", user);
+
+  if (!user) {
+    res.status(400);
+    throw new Error("User not found, please sign up");
+  }
+
+  // User exists, check if password is correct
+  const passwordIsCorrect = await bcrypt.compare(password, user.password);
+  console.log(passwordIsCorrect, "passwordIsCorrect");
+
+  // Generate jwt token
+  const token = generateToken(user._id);
+
+  // Send token with http only cookie
+  res.cookie("token", token, {
+    path: "/",
+    httpOnly: true,
+    expires: new Date(Date.now() + 1000 * 86400), // 1day
+    sameSite: "none",
+    secure: false,
+  });
+
+  if (user && passwordIsCorrect) {
+    const { _id, name, email, photo, phone, bio } = user;
+    res.status(200).json({
+      _id,
+      name,
+      email,
+      photo,
+      phone,
+      bio,
+      token,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid email or password");
+  }
 });
 
 module.exports = {
