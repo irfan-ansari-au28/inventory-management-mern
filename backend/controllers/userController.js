a206171376195e9367295bc5617e3d71647dc2fd
apple <apple@apples-MacBook-Pro.local>
2023-01-04 18:58:09 +0530

    generate token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,12 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+// Generate token
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
@@ -29,6 +36,9 @@ const registerUser = asyncHandler(async (req, res) => {
     password,
   });
 
+  // Generate jwt token
+  const token = generateToken(user._id);
+
   if (user) {
     const { _id, name, email, photo, phone, bio } = user;
     res.status(201).json({
@@ -38,6 +48,7 @@ const registerUser = asyncHandler(async (req, res) => {
       photo,
       phone,
       bio,
+      token,
     });
   } else {
     res.status(400);
