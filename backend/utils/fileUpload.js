f110dcac28d202b3f366c5c4ac86f1a9ee293f60
apple <apple@apples-MacBook-Pro.local>
2023-01-06 15:08:40 +0530

    setup multer file upload

---
+++ backend/utils/fileUpload.js
@@ -0,0 +1,28 @@
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.toISOString().replace(/:/g, "-")) + file.originalname;
+  },
+});
+
+// Specify file formats that can be saved
+
+function fileFilter(req, file, cb) {
+  if (
+    file.mimeType === "image/png" ||
+    file.mimeType === "image/jpg" ||
+    file.mimeType === "image/jpeg"
+  ) {
+    cb(null, false);
+  } else {
+    cb(null, true);
+  }
+}
+
+const upload = multer({ storage, fileFilter });
+
+module.exports = upload;
