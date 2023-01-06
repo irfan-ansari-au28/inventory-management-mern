9a47cea03e5013f99bea5ab00886b4e0073b44d8
apple <apple@apples-MacBook-Pro.local>
2023-01-06 18:53:12 +0530

    create contact controller

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -1,7 +1,35 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const sendEmail = require("../utils/sendEmail");
 
 const contactUs = asyncHandler(async (req, res) => {
-  res.send("contactus");
+  const { subject, message } = req.body;
+  const user = await User.findById(req.user._id);
+
+  if (!user) {
+    res.status(400);
+    throw new Error("User not found, please signup");
+  }
+
+  // Validation
+  if (!subject || !message) {
+    res.status(400);
+    throw new Error("Please add subject and message");
+  }
+
+  const send_to = process.env.EMAIL_USER;
+  const sent_from = process.env.EMAIL_USER;
+
+  try {
+    await sendEmail(sent_from, send_to, subject, message);
+    res.status(200).json({
+      success: true,
+      message: "Reset Email Sent",
+    });
+  } catch (error) {
+    res.status(500);
+    throw new Error("Email not sent, please try again");
+  }
 });
 
 module.exports = { contactUs };
