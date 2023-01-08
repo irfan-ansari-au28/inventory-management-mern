688cb4fb25e509b0f2a45458962fb414fbccaddf
apple <apple@apples-MacBook-Pro.local>
2023-01-08 16:16:27 +0530

    add sidebar data

--- frontend/src/components/sidebar/Sidebar.js
+++ frontend/src/components/sidebar/Sidebar.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { HiMenuAlt3 } from "react-icons/hi";
+import { RiProductHuntLine } from "react-icons/ri";
+import SidebarItem from "./SidebarItem";
+import menu from "../../data/sidebar";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  const navigate = useNavigate();
+  const goHome = () => {
+    navigate("/");
+  };
   return (
     <>
       <div
-        className={
-          "bg-gradient-to-br p-4 from-blue-gray-800 to-blue-gray-900 -translate-x-80 fixed inset-0 z-50 my-4 ml-4 h-[calc(100vh-32px)] w-72 rounded-xl transition-transform duration-300 xl:translate-x-0"
-        }
+        className={`${""} bg-gradient-to-br p-4 from-blue-gray-300 to-blue-gray-200 -translate-x-80 fixed inset-0 z-50 my-4 ml-4 h-[calc(100vh-32px)] w-72 rounded-xl transition-transform duration-300 xl:translate-x-0`}
       >
-        Sidebar
+        <div className="flex items-center justify-between">
+          <div className="" style={{}} onClick={goHome}>
+            <RiProductHuntLine size={35} style={{ cursor: "pointer" }} />
+          </div>
+
+          <div className="" style={{ cursor: "pointer" }}>
+            <HiMenuAlt3 />
+          </div>
+        </div>
+        {menu.map((item) => {
+          return <SidebarItem key={Math.random() * 100} item={item} />;
+        })}
       </div>
       <main>{children}</main>
     </>
--- frontend/src/components/sidebar/SidebarItem.js
+++ frontend/src/components/sidebar/SidebarItem.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { Typography } from "@material-tailwind/react";
+import { Link } from "react-router-dom";
 
-const SidebarItem = () => {
-  return <div>SidebarItem</div>;
+const SidebarItem = ({ item }) => {
+  if (item.children) {
+    <div>
+      {item.children.map((child, index) => {
+        return (
+          <div key={index}>
+            <Link to={child.path}>
+              <div>{child.title}</div>
+            </Link>
+          </div>
+        );
+      })}
+    </div>;
+  } else {
+    return (
+      <Link to={item.path}>
+        <ul key={item.title} className="mb-4 flex flex-col gap-1">
+          <li className="mx-3.5 mt-4 mb-2 flex items-center justify-center ">
+            {item.icon}
+            <Typography
+              variant="small"
+              color={"white"}
+              className="font-black uppercase opacity-75 ml-2"
+            >
+              {item.title}
+            </Typography>
+          </li>
+        </ul>
+      </Link>
+    );
+  }
 };
 
 export default SidebarItem;
