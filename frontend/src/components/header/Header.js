c18609658a270f817e5f6fd41ae8ffa41d0da0d6
apple <apple@apples-MacBook-Pro.local>
2023-01-09 15:26:13 +0530

    logut complete

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,7 +1,19 @@
 import { Button } from "@material-tailwind/react";
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { SET_LOGIN } from "../../redux/features/auth/authSlice";
+import { logoutUser } from "../../services/authService";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const logout = async () => {
+    await logoutUser();
+    await dispatch(SET_LOGIN(false));
+    navigate("/login");
+  };
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center justify-center">
@@ -9,7 +21,7 @@ const Header = () => {
         <div>User</div>
       </div>
       <div>
-        <Button variant="gradient" fullWidth>
+        <Button variant="gradient" onClick={logout} fullWidth>
           Logout
         </Button>
       </div>
