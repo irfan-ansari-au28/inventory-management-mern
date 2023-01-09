36e847d9d4b95e3e2c71b73631bc5f3df6c6ec36
apple <apple@apples-MacBook-Pro.local>
2023-01-09 15:32:11 +0530

    display user name

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,13 +1,14 @@
 import { Button } from "@material-tailwind/react";
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { SET_LOGIN } from "../../redux/features/auth/authSlice";
+import { selectName, SET_LOGIN } from "../../redux/features/auth/authSlice";
 import { logoutUser } from "../../services/authService";
 
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const name = useSelector(selectName);
 
   const logout = async () => {
     await logoutUser();
@@ -18,7 +19,7 @@ const Header = () => {
     <div className="flex items-center justify-between">
       <div className="flex items-center justify-center">
         <div>Welcome</div>
-        <div>User</div>
+        <div className="ml-2">{name}!</div>
       </div>
       <div>
         <Button variant="gradient" onClick={logout} fullWidth>
