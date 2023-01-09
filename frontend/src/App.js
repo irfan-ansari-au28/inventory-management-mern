eb0d941fad16234d38d4bbc3d607192344df81b2
apple <apple@apples-MacBook-Pro.local>
2023-01-09 17:29:55 +0530

    get login status

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
 import Home from "./pages/Home/Home";
 import axios from "axios";
+// import { useDispatch } from "react-redux";
+// import { useEffect } from "react";
+// import { getLoginStatus } from "./services/authService";
+// import { SET_LOGIN } from "./redux/features/auth/authSlice";
 
 axios.defaults.withCredentials = true;
 function App() {
-  // eslint-disable-next-line react/react-in-jsx-scope
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   async function loginStatus() {
+  //     const status = await getLoginStatus();
+  //     console.log("status", status);
+  //     dispatch(SET_LOGIN(status));
+  //   }
+  //   loginStatus();
+  // }, [dispatch]);
+
   return <Home />;
 }
 
