6a94e7cd55e1655a5c68a8e910fa9688b7fd3374
apple <apple@apples-MacBook-Pro.local>
2023-01-09 10:59:35 +0530

    Loader portal

---
+++ frontend/src/loader/Loader.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { RotatingLines } from "react-loader-spinner";
+import ReactDOM from "react-dom";
+
+const Loader = () => {
+  return ReactDOM.createPortal(
+    <div
+      style={{
+        backgroundColor: "#00000040",
+        width: "100%",
+        height: "100vh",
+        position: "fixed",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        zIndex: 20,
+      }}
+    >
+      <RotatingLines
+        strokeColor="red"
+        strokeWidth="5"
+        animationDuration="0.75"
+        width="96"
+        visible={true}
+      />
+    </div>,
+    document.getElementById("loader")
+  );
+};
+
+export const Spinner = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <RotatingLines
+        strokeColor="red"
+        strokeWidth="5"
+        animationDuration="0.75"
+        width="24"
+        visible={true}
+      />
+    </div>
+  );
+};
+
+export default Loader;
--- frontend/src/pages/auth/Register.js
+++ frontend/src/pages/auth/Register.js
@@ -67,84 +67,88 @@ const Register = () => {
     }
   };
   return (
-    <div className="container mx-auto p-4">
-      <Card className="absolute top-2/4 left-2/4 w-full max-w-[24rem] -translate-y-2/4 -translate-x-2/4">
-        <form onSubmit={register}>
-          <CardHeader
-            variant="gradient"
-            color="blue"
-            className="mb-4 grid h-28 place-items-center"
-          >
-            <Typography variant="h3" color="white">
-              Sign Up
-            </Typography>
-          </CardHeader>
-          <CardBody className="flex flex-col gap-4">
-            <div className="mb-2">
-              <Input
-                label="Name"
-                name="name"
-                value={name}
-                size="lg"
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="mb-2">
-              <Input
-                type="email"
-                name="email"
-                value={email}
-                label="Email"
-                size="lg"
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="mb-2">
-              <Input
-                type="password"
-                name="password"
-                value={password}
-                label="Password"
-                size="lg"
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="mb-2">
-              <Input
-                type="password"
-                name="password2"
-                value={password2}
-                label="Confirm Password"
-                size="lg"
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="-ml-2.5">
-              <Checkbox label="I agree the Terms and Conditions" />
-            </div>
-          </CardBody>
-          <CardFooter className="pt-0">
-            <Button type="submit" variant="gradient" fullWidth>
-              Sign Up
-            </Button>
+    <>
+      <div className="container mx-auto p-4">
+        {isLoading && <Loader />}
 
-            <Typography variant="small" className="mt-6 flex justify-center">
-              Already have an account?
-              <Link to="/login">
-                <Typography
-                  as="span"
-                  variant="small"
-                  color="blue"
-                  className="ml-1 font-bold"
-                >
-                  Sign in
-                </Typography>
-              </Link>
-            </Typography>
-          </CardFooter>
-        </form>
-      </Card>
-    </div>
+        <Card className="absolute top-2/4 left-2/4 w-full max-w-[24rem] -translate-y-2/4 -translate-x-2/4">
+          <form onSubmit={register}>
+            <CardHeader
+              variant="gradient"
+              color="blue"
+              className="mb-4 grid h-28 place-items-center"
+            >
+              <Typography variant="h3" color="white">
+                Sign Up
+              </Typography>
+            </CardHeader>
+            <CardBody className="flex flex-col gap-4">
+              <div className="mb-2">
+                <Input
+                  label="Name"
+                  name="name"
+                  value={name}
+                  size="lg"
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="mb-2">
+                <Input
+                  type="email"
+                  name="email"
+                  value={email}
+                  label="Email"
+                  size="lg"
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="mb-2">
+                <Input
+                  type="password"
+                  name="password"
+                  value={password}
+                  label="Password"
+                  size="lg"
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="mb-2">
+                <Input
+                  type="password"
+                  name="password2"
+                  value={password2}
+                  label="Confirm Password"
+                  size="lg"
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="-ml-2.5">
+                <Checkbox label="I agree the Terms and Conditions" />
+              </div>
+            </CardBody>
+            <CardFooter className="pt-0">
+              <Button type="submit" variant="gradient" fullWidth>
+                Sign Up
+              </Button>
+
+              <Typography variant="small" className="mt-6 flex justify-center">
+                Already have an account?
+                <Link to="/login">
+                  <Typography
+                    as="span"
+                    variant="small"
+                    color="blue"
+                    className="ml-1 font-bold"
+                  >
+                    Sign in
+                  </Typography>
+                </Link>
+              </Typography>
+            </CardFooter>
+          </form>
+        </Card>
+      </div>
+    </>
   );
 };
 
