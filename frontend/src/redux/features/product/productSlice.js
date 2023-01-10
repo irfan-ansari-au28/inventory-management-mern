9a060596a82fb976c02fb64f0c21b8fb3c7cfb52
apple <apple@apples-MacBook-Pro.local>
2023-01-10 09:56:30 +0530

    create product slice

---
+++ frontend/src/redux/features/product/productSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+const initialState = {
+  product: null,
+  products: [],
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
+
+const productSlice = createSlice({
+  name: "product",
+  initialState,
+  reducers: {
+    CALC_STORE_VALUE: (state, action) => {
+      console.log("store value");
+    },
+  },
+  extraReducers: (builder) => {
+    console.log("extra reducer");
+  },
+});
+
+export const { CALC_STORE_VALUE } = productSlice.actions;
+
+export default productSlice.reducer;
